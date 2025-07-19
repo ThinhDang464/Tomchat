742555f4afdcbda19b51f07a430ee47f57e2dbc5
ThinhDang464 <89332458+ThinhDang464@users.noreply.github.com>
2025-07-20 01:11:59 +1000

    server both express and react same url

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,10 +6,13 @@ import chatRoutes from "./routes/chat.route.js";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import path from "path";
 //----------------------------------EXPRESS + ENV-----------------
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -24,6 +27,15 @@ app.use(cookieParser()); //access cookies inside request
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+//both serve api adn react app
+if (process.env.NODE_ENV === "production") {
+  //fromt current directory go 1 above to frontend and dist folder
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server Runnin port ${PORT}`);
